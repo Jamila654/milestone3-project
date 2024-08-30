741547a1d04e46d9f44a9623bde89da38c67b3a9
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2024-08-30 15:11:30 +0500

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <main className=" fixed bottom-0 right-0 left-0 w-full h-14 sm:h-24 bg-black text-white text-nowrap flex items-center justify-around">
+        <main className=" bottom-0 right-0 left-0 w-full h-14 sm:h-24 bg-black text-white text-nowrap flex items-center justify-around">
             <p>&copy; {new Date().getFullYear()} Jam's Closet. All rights reserved.</p>
         </main>
      );
